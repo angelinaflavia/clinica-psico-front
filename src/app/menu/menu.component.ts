714e7b0ca8e62b805899602e55fef67ddb5abcaf
Flávia Angelina <flavia.angelinal@gmail.com>
2018-06-06 22:16:58 -0300

    atualização

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ListaEstagiarioComponent } from './../lista-estagiario/lista-estagiario.component';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
